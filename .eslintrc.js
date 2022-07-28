3fd71f74b8de99e7cac61478b55a68ecdc47a940
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-07-28 10:57:01 -0400

    Create ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  }
+};
