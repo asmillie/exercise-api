f0190a4c148b49ed7d3e556dee90f7edc3512d1d
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-08-05 12:41:16 -0400

    Create users middleware

---
+++ users/middleware/users.middleware.ts
@@ -0,0 +1,82 @@
+import express, { NextFunction, Request, Response } from 'express';
+import debug from 'debug';
+import usersService from '../services/users.service';
+
+const log: debug.IDebugger = debug('app:users-middleware');
+
+class UsersMiddleware {
+    async validateRequiredUserBodyFields(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        if (req.body && req.body.email && req.body.password) {
+            next();
+        } else {
+            res.status(400).send({
+                error: 'Missing required fields email and password'
+            });
+        }
+    }
+
+    async validateUniqueEmail(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        const user = await usersService.getUserByEmail(req.body.email);
+        if (user) {
+            res.status(400).send({
+                error: 'User email already exists'
+            });
+        } else {
+            next();
+        }
+    }
+
+    async validateEmailBelongsToUser(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        const user = await usersService.getUserByEmail(req.body.email);
+        if (user && user.id === req.params.userId) {
+            next();
+        } else {
+            res.status(400).send({
+                error: 'Invalid email'
+            });
+        }
+    }
+
+    validatePatchEmail = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        if (req.body.email) {
+            log('Validating email', req.body.email);
+
+            this.validateEmailBelongsToUser(req, res, next);
+        } else {
+            next();
+        }
+    }
+
+    async validateUserExists(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        const user = await usersService.readById(req.params.userId);
+        if (user) {
+            next();
+        } else {
+            res.status(404).send({
+                error: `User ${req.params.userId} not found`
+            });
+        }
+    }
+}
+
+export default new UsersMiddleware();
